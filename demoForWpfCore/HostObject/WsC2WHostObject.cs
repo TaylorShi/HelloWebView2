87e211ae1841898645f07cbdae8592e9bfc2809a
shiya <shiya@300264.cn>
2021-10-27 14:28:04 +0800

    从AddHostObjectToScript方法公开被Javascript调用的本机方法

---
+++ demoForWpfCore/HostObject/WsC2WHostObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoForWpfCore.HostObject
+{
+    #pragma warning disable CS0618
+    [System.Runtime.InteropServices.ClassInterface(System.Runtime.InteropServices.ClassInterfaceType.AutoDual)]
+    #pragma warning restore CS0618
+    [System.Runtime.InteropServices.ComVisible(true)]
+    public class WsC2WHostObject
+    {
+        public void WSClientFunction(string requestInfo)
+        {
+            Console.WriteLine(requestInfo);
+        }
+    }
+}
